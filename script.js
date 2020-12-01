0e6fd2b9546c5e236afd1100b964957d18882776
Andrii Mishtal <mishtal.andriy@gmail.com>
2020-12-01 17:57:25 +0200

    Finish project

--- script.js
+++ script.js
@@ -32,3 +32,107 @@ const words = [
   'loving'
 ];
 
+// Init word
+let randomWord;
+// Init score
+let score = 0;
+
+// Init time
+let time = 10;
+
+// Set difficult to value in local storage
+let difficulty = localStorage.getItem('difficulty') !== null 
+  ? localStorage.getItem('difficulty') 
+  : 'medium';
+
+// Set difficulty select value
+difficultySelect.value = localStorage.getItem('difficulty') !== null 
+  ? localStorage.getItem('difficulty') 
+  : 'medium';
+
+
+// Focus on text on start
+text.focus();
+
+// Start counting down
+const timeInterval = setInterval(updateTime, 1000)
+
+// Get random word from array
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)]
+}
+
+
+// Add word to DOM
+function addWordToDom() {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
+}
+
+// Update score
+function updateScore() {
+  score++;
+  scoreEl.innerHTML = score            
+}
+
+// Update time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + 's';
+
+  if(time === 0) {
+    clearInterval(time);
+    // End game
+    gameOver();
+  }
+}
+
+// Game over function show  end screen
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+  `;
+
+  endgameEl.style.display = 'flex'
+}
+
+addWordToDom();
+
+// Event listeners
+
+// Typing
+text.addEventListener('input', e => {
+  const insertText = e.target.value;
+  
+  if(insertText === randomWord) {
+    addWordToDom();
+    updateScore();
+
+    //Clear input
+    e.target.value = '';
+
+    if (difficulty === 'hard') {
+      time+=2;
+    } else if(difficulty === 'medium') {
+      time+=3;
+    } else {
+      time+=5;
+    }
+
+    updateTime();
+  } 
+})
+
+// Settings btn click
+settingsBtn.addEventListener('click', () => {
+  settings.classList.toggle('hide')
+})
+
+// Settings select
+settingsForm.addEventListener('change', e => {
+  difficulty = e.target.value;
+  console.log(difficulty);
+  localStorage.setItem('difficulty', difficulty)
+})
